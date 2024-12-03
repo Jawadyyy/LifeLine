940398cb7c954f4bde718bd30af103797f8708ce
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2024-12-03 11:21:49 +0500

    added bottom nav bar and turn on internet dialog box in mapscreen

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:lifeline/screens/homepage.dart';
 import 'package:lifeline/services/location_handler.dart';
 
 class MapScreen extends StatefulWidget {
@@ -15,11 +16,49 @@ class _MapScreenState extends State<MapScreen> {
   final MapController _mapController = MapController();
   LatLng? _currentPosition;
   String? _currentAddress;
+  int _selectedIndex = 2;
 
   @override
   void initState() {
     super.initState();
+    _checkLocationServiceAndLoadLocation();
+  }
+
+  Future<void> _checkLocationServiceAndLoadLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      _showLocationServiceDialog();
+      return;
+    }
     _loadCurrentLocation();
+  }
+
+  Future<void> _showLocationServiceDialog() async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Location Service Disabled'),
+          content: const Text(
+              'Your location services are turned off. Please enable them to use the map.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.of(context).pop();
+                await Geolocator.openLocationSettings();
+              },
+              child: const Text('Enable'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   Future<void> _loadCurrentLocation() async {
@@ -52,7 +91,7 @@ class _MapScreenState extends State<MapScreen> {
           : FlutterMap(
               mapController: _mapController,
               options: MapOptions(
-                initialCenter: _currentPosition ?? LatLng(0.0, 0.0),
+                initialCenter: _currentPosition ?? const LatLng(0.0, 0.0),
                 initialZoom: 15.0,
               ),
               children: [
@@ -63,7 +102,7 @@ class _MapScreenState extends State<MapScreen> {
                 MarkerLayer(
                   markers: [
                     Marker(
-                      point: _currentPosition ?? LatLng(0.0, 0.0),
+                      point: _currentPosition ?? const LatLng(0.0, 0.0),
                       width: 50.0,
                       height: 50.0,
                       child: const Icon(
@@ -77,8 +116,49 @@ class _MapScreenState extends State<MapScreen> {
               ],
             ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _loadCurrentLocation,
+        onPressed: _checkLocationServiceAndLoadLocation,
         child: const Icon(Icons.my_location),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: "My circle",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: "Map",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+        onTap: (index) {
+          if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MapScreen()),
+            );
+          } else if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );
+          } else {
+            setState(() {
+              _selectedIndex = index;
+            });
+          }
+        },
+        selectedItemColor: const Color.fromARGB(255, 32, 34, 160),
+        unselectedItemColor: Colors.grey,
       ),
     );
   }
